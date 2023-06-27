d4a39f2104ad43185d05b516a5d7f47f456200fd
ercelserhat <ercelserhat@gmail.com>
2023-06-27 18:34:09 +0300

    Saving data to Firestore

--- PhotoSharingApp_WithFirebase/UploadViewController.swift
+++ PhotoSharingApp_WithFirebase/UploadViewController.swift
@@ -6,6 +6,10 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuth
+import FirebaseStorage
+import FirebaseFirestore
 
 class UploadViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
@@ -34,8 +38,44 @@ class UploadViewController: UIViewController, UIImagePickerControllerDelegate, U
         
     }
     
-
     @IBAction func uploadButonTiklandi(_ sender: Any) {
+        let storage = Storage.storage()
+        let storageReference = storage.reference()
+        let mediaFolder = storageReference.child("media")
+        if let data = imageView.image?.jpegData(compressionQuality: 0.5){
+            let uuid = UUID().uuidString
+            let imageReference = mediaFolder.child("\(uuid).jpg")
+            imageReference.putData(data) { storagemetadata, error in
+                if error != nil{
+                    self.hataMesaji(title: "HATA", message: error?.localizedDescription ?? "Hata! Tekrar Deneyin.")
+                }else{
+                    imageReference.downloadURL { url, error in
+                        if error == nil{
+                            let imageUrl = url?.absoluteString
+                            if let imageUrl = imageUrl{
+                                let firestoreDatabase = Firestore.firestore()
+                                let firestorePost = ["gorselurl" : imageUrl, "yorum" : self.yorumTextField.text!, "email" : Auth.auth().currentUser!.email, "tarih" : FieldValue.serverTimestamp()] as [String : Any]
+                                firestoreDatabase.collection("Post").addDocument(data: firestorePost) { error in
+                                    if error != nil{
+                                        self.hataMesaji(title: "HATA!", message: error?.localizedDescription ?? "Hata! Tekrar Deneyin.")
+                                    }else{
+                                        
+                                    }
+                                }
+                            }
+                            
+                           
+                        }
+                    }
+                }
+            }
+        }
     }
     
+    func hataMesaji(title: String, message: String){
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okButton = UIAlertAction(title: "OK", style: .default)
+        alert.addAction(okButton)
+        self.present(alert, animated: true)
+    }
 }
